f246e59f06e579da4eeff9f4b8fe5b2e924b447b
Akshay-hub-007 <akshaykalangi9@gmail.com>
2025-11-15 12:20:56 +0530

    implement search and filter functionality for retrieving blogs

--- services/blog/src/controller/blog.ts
+++ services/blog/src/controller/blog.ts
@@ -3,14 +3,42 @@ import type { AuthenticatedRequest } from "../middleware/isAuth.js";
 import TryCatch from "../utils/TryCatch.js";
 import { sql } from "../utils/db.js";
 
-
 export const getAllBlogs = TryCatch(async (req, res) => {
-  const blogs = await sql`
-    SELECT * FROM blogs 
-    ORDER BY created_at DESC
-  `;
+  const { searchQuery, category } = req.query;
 
-  return res.json({
-    blogs,
-  });
+  const query = searchQuery ? `%${searchQuery}%` : null;
+
+  let blogs;
+
+  if (searchQuery && category) {
+    blogs = await sql`
+      SELECT * FROM blogs 
+      WHERE (title ILIKE ${query} OR description ILIKE ${query})
+      AND category = ${category}
+      ORDER BY created_at DESC
+    `;
+  } 
+  else if (searchQuery) {
+    blogs = await sql`
+      SELECT * FROM blogs 
+      WHERE title ILIKE ${query} 
+      OR description ILIKE ${query}
+      ORDER BY created_at DESC
+    `;
+  } 
+  else if (category) {
+    blogs = await sql`
+      SELECT * FROM blogs 
+      WHERE category = ${category}
+      ORDER BY created_at DESC
+    `;
+  } 
+  else {
+    blogs = await sql`
+      SELECT * FROM blogs 
+      ORDER BY created_at DESC
+    `;
+  }
+
+  return res.json({ blogs });
 });
